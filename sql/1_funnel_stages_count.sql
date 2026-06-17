48c0feb7e5c86a940863a354aa0a729a3d17a8d4
Sigmus Aimofumhe <eshiobomhesigmusaimofumhe04@gmail.com>
2026-06-17 15:31:22 -0400

    Updated README.md

---
+++ sql/1_funnel_stages_count.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE user_events (
+--     event_id INT PRIMARY KEY,
+--     user_id INT,
+--     event_type VARCHAR(50),
+--     event_date TIMESTAMP,
+--     product_id INT,
+--     amount NUMERIC,
+--     traffic_source VARCHAR(50)
+-- );
+
